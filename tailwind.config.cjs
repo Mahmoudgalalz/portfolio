f103f9a506860aabab77f7855248b66e22f4c6c3
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-03-20 03:33:09 +0200

    :lipstick: custom colors tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors:{
+			'main':'#F9F5EB',
+			'prime':'#002B5B',
+			'sub-main':'#E4DCCF',
+			'accent':'#EA5455',
+		},
+		container:{
+			center:true,
+			padding:'2rem',
+		},
 		extend: {},
 	},
 	plugins: [],
